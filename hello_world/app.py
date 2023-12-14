65446bd4ce93bf6137326bc32c7297ad31fddb30
zeelz <keme.kenneth@gmail.com>
2023-12-14 02:00:47 +0100

    updated

--- hello_world/app.py
+++ hello_world/app.py
@@ -32,12 +32,12 @@ def lambda_handler(event, context):
     #     print(e)
 
     #     raise e
-    print(event['body'])
+    # print(event['body'])
+    data = event["city"]
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "data": event['body'],
-            # "message": "hello world",
+            "message": "hello world",
             # "location": ip.text.replace("\n", "")
         }),
     }
